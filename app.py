7f9d9a65561e6a4eb737847fbe680233a6bfaac0
Sai Yashwanth <taddishetty34@gmail.com>
2024-07-18 00:17:52 +0530

    prompt changes

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ twitter_tool = tweet
 
 os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
 
-
 gpt = ChatOpenAI(model_name="gpt-4o", temperature=0.9)
 
 research_agent = Agent(
@@ -26,7 +25,9 @@ research_agent = Agent(
 
 tweet_agent = Agent(
     role='Tweet Creator',   
-    goal='Create and post tweets based on research, To increase engagement of the account and to teach users latest trends in AI',
+    goal="""
+    Create and post tweets based on research, To increase engagement of the account and to teach users AI
+    """,
     tools=[twitter_tool],
     backstory='A creative writer who can craft engaging tweets based on the latest research.',
     llm = gpt
@@ -36,11 +37,16 @@ tweet_agent = Agent(
 
 # Research task
 research_task = Task(
-    description="""Use Exa to research the latest trends in AI. 
-    Research should be latest (One day day), should be related to Companies, fundings, academic papers, influential personnels
-    Research should be helpful for anyone who wants to stay updated and survive in this age of AI
+    description="""Use Exa to research the latest trends in AI/LLM/AI Agents. 
+    Research should be latest (past week), Should be about AI news.
+    Research should be helpful for anyone who wants to stay updated and survive in this age of AI/LLM/AI Agents
     """,
-    expected_output="A summary of the latest trends in AI.",
+    expected_output="""A key summary of the latest news in AI/LLM/AI Agents.
+    Should include:
+    - One line about an Influential Person in AI
+    - Latest AI companies fundings
+    - Latest AI research
+    """,
     agent=research_agent
 )
 
@@ -52,17 +58,18 @@ tweet_task = Task(
     Ensure the character limit is 280 characters
     """,
     expected_output="""
-    A tweet posted using the tool summarizing the latest trends in AI. 
+    A tweet posted using the tool summarizing the latest trends in AI/LLMs/AI Agents. 
     Ensure the character limit is 280 characters
-    
-    <Example>
-    Expected formated:Top stories in AI today:
-    -AI breakthrough improves Alzheimer's predictions
-    -YouTube Music gets new AI features
-    -Generate beautiful presentations with AI
-    -Microsoft gives AI a spreadsheet boost
-    -6 new AI tools & 4 new AI jobs
-    </Example>
+
+    <Example Tweet>
+    Expected formated:
+    Hook to engage audience
+    bullet point summary (Around 3-4)
+    </Example Tweet>
+
+    Important: Tweet is posted using the tweet function tool. 
+    The tool takes an argument content which is basically the tweet content. 
+    Pass the content before using the tool. Very important!
     """,
     agent=tweet_agent,
     context=[research_task]
@@ -79,8 +86,9 @@ crew = Crew(
 import time
 
 while True:
-  # Execute the desired line
-  result = crew.kickoff(inputs={'topic': 'latest technology trends'})
-  
-  # Wait for 2 hours (2 hours * 60 minutes/hour * 60 seconds/minute)
-  time.sleep(2 * 60 * 60)
+    # Execute the desired line
+    result = crew.kickoff(inputs={'topic': 'latest AI/LLM/AI Agents trends'})
+    print(result)
+
+    # Wait for 2 hours (2 hours * 60 minutes/hour * 60 seconds/minute)
+    time.sleep(2 * 60 * 60)
